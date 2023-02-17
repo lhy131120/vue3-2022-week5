8818169136c183d4341ed7bebb4dccf83791a6f0
sakimotorin <sakimotorin@gmail.com>
2023-02-17 12:18:35 +0800

    finish cart function

--- js/cart.js
+++ js/cart.js
@@ -3,8 +3,9 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.45/vue
 const domain = 'https://vue3-course-api.hexschool.io/v2'
 const api_path = 'sakimotorin-vue2022'
 
+// 元件
 const productModal = {
-  props: ['id', 'addToCart'],
+  props: ['id', 'addToCart', 'openModal'],
   data() {
     return {
       tempProduct: {},
@@ -15,16 +16,21 @@ const productModal = {
   template: '#userProductModal',
   mounted() {
     this.modal = new bootstrap.Modal(this.$refs.modal)
+    this.$refs.modal.addEventListener('hidden.bs.modal', (event) => {
+      this.openModal('')
+    })
   },
   watch: {
     // 監聽prop的資料
     id() {
-      console.log(`watch回來的 Id: ${this.id}`)
-      axios.get(`${domain}/api/${api_path}/product/${this.id}`).then((res) => {
-        console.log('單一產品', res.data.product)
-        this.tempProduct = res.data.product
-        this.showModal()
-      })
+      // console.log(`watch回來的 Id: ${this.id}`)
+      if(this.id) {
+        axios.get(`${domain}/api/${api_path}/product/${this.id}`).then((res) => {
+          // console.log('單一產品', res.data.product)
+          this.tempProduct = res.data.product
+          this.showModal()
+        })
+      }
     },
   },
   methods: {
@@ -37,12 +43,15 @@ const productModal = {
   },
 }
 
+// 根元件
 const vm = createApp({
   data() {
     return {
       products: [],
       productId: '',
       cart: {},
+      loadingItem: '',
+      clearAll: false,
     }
   },
   components: {
@@ -51,52 +60,62 @@ const vm = createApp({
   methods: {
     getProducts() {
       axios.get(`${domain}/api/${api_path}/products/all`).then((res) => {
-        console.log('產品列表', res.data.products)
+        // console.log('產品列表', res.data.products)
         this.products = res.data.products
       })
     },
     openModal(id) {
       this.productId = id
-      console.log(`外層帶入 productId: ${id}`)
+      // console.log(`外層帶入 productId: ${id}`)
     },
     addToCart(product_id, qty = 1) {
+      this.loadingItem = product_id
       const data = { product_id, qty }
       axios.post(`${domain}/api/${api_path}/cart`, { data }).then((res) => {
-        console.log(res.data)
-        setTimeout(() => {
-          this.$refs.productModal.hideModal()
-        }, 300)
+        this.loadingItem = ''
+        alert(res.data.message)
+        this.$refs.productModal.hideModal()
         this.getCarts()
       })
     },
     getCarts() {
       axios.get(`${domain}/api/${api_path}/cart`).then((res) => {
-        console.log('購物車:', res.data.data)
+        // console.log('購物車:', res.data.data)
         this.cart = res.data.data
       })
     },
     modifyItemQty(item) {
+      this.loadingItem = item.id
       const data = {
         product_id: item.product.id,
         qty: item.qty,
       }
-      // console.log(data, item.id);
       axios
         .put(`${domain}/api/${api_path}/cart/${item.id}`, { data })
         .then((res) => {
-          console.log('更新後的產品', res.data)
+          alert(res.data.message)
+          this.loadingItem = ''
           this.getCarts()
         })
     },
     deleteItem(item) {
-      axios.delete(`${domain}/api/${api_path}/cart/${item.id}`).then((res) => {
-        console.log(res.data)
-        this.getCarts()
-      })
+      this.loadingItem = item.id
+      axios
+        .delete(`${domain}/api/${api_path}/cart/${item.id}`)
+        .then((res) => {
+          this.loadingItem = ''
+          alert(res.data.message)
+          this.getCarts()
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+        })
     },
     deleteAllItem() {
+      this.clearAll = true
       axios.delete(`${domain}/api/${api_path}/carts`).then((res) => {
-        console.log(res.data)
+        alert(res.data.message)
+        this.clearAll = false
         this.getCarts()
       })
     },
